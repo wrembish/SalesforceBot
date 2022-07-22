85c0145b212695fc78e094bea02ed654f1275c0b
wrembish <wrembish@udel.edu>
2022-07-22 19:47:48 -0400

    Clean up on messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -7,48 +7,38 @@ module.exports = {
 
         const helpers = require('../helperFunctions.js')
         const sf = message.client.sf
-        let queryStr = `SELECT+Id,Name+from+GuildId__c`
-        let records = await helpers.soql(queryStr, sf)
+        
         const currGuildId = message.guild.id
+        const guildIdQuery = `SELECT+Id,Name+from+GuildId__c`
+        const guildIdResult = await helpers.soql(guildIdQuery, sf)
+
         let isIn = false
-        if(records.totalSize >= 0) { records.records.forEach((record) => { if(record.Name === currGuildId) isIn = true; }) }
-        if(!isIn) {
-            const body = {
-                "Name" : currGuildId
-            }
-            await helpers.insert('GuildId__c', body, sf)
-        }
+        if(guildIdResult.totalSize >= 0) { guildIdResult.records.forEach((record) => { if(record.Name === currGuildId) isIn = true; }) }
+        if(!isIn) { await helpers.insert('GuildId__c', { Name : currGuildId }, sf) }
 
         const content = message.content
-
-        if(content.startsWith(COMMAND_CHAR)) {
-            if(content.split(' ').length <= 1 && content.split(COMMAND_CHAR).length > 1) {
-                const dieCountName = content.split(COMMAND_CHAR)[1];
-                if(dieCountName.length < 2) return;
-                queryStr = `SELECT+Id,Name,Count__c+from+DieCount__c+WHERE+Name='${dieCountName}'+LIMIT+1`
-                records = await helpers.soql(queryStr, sf)
+        
+        if(content.startsWith(COMMAND_CHAR) && content !== COMMAND_CHAR) {
+            if(content.split(' ').length === 1 && content.length > 2) {
+                const dieCountName = content.substring(1)
+                const queryStr = `SELECT+Id,Name,Count__c+from+DieCount__c+WHERE+Name='${dieCountName}'+LIMIT+1`
+                const result = await helpers.soql(queryStr, sf)
 
                 let reply = ''
-                let body = {
-                    'Name'  : dieCountName
-                }
-                if(records.totalSize <= 0) {
-                    reply += `Congrats ${dieCountName} you now have 1 :electric_plug: Disconnet :electric_plug:`
+                let body = { Name  : dieCountName }
+
+                if(result.totalSize === 0) {
+                    reply = `Congrats ${dieCountName} you now have 1 :electric_plug: Disconnet :electric_plug:`
                     body.Count__c = 1
                 } else {
-                    const count = records.records[0].Count__c+1
-                    reply += `Current Toll for ${dieCountName}: ${count}`
-                    body.Count__c = count
+                    body.Count__c = result.records[0].Count__c+1
+                    reply = `Current Toll for ${dieCountName}: ${body.Count__c}`
                 }
 
                 await helpers.upsert('DieCount__c', 'Ext_Id__c', dieCountName, body, sf)
 
                 await message.channel.send(reply)
-            } else if(content.split(' ').length === 2 && content.split(COMMAND_CHAR).length > 1) {
-
             }
-
-            // if(content === '!!ping') await message.channel.send('Pong!')
         }
     },
 }
